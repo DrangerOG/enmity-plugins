84b4bc7ca782b3c8eb2e394b7cef66ea911a56ee
DrangerOG <145707265+DrangerOG@users.noreply.github.com>
2025-04-14 14:24:39 -0700

    Update index.ts

--- NoDelete/src/commands/index.ts
+++ NoDelete/src/commands/index.ts
@@ -1,5 +1,7 @@
-import { logs } from "./logs";
+import { logs } from "./logs"; // Import logs command
 
+// Define the list of commands including the logs command
 const commands = [logs];
 
+// Export commands so they can be used elsewhere in the plugin
 export { commands };
