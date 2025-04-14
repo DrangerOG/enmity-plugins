181ddebbf311d5c0f40a58561e8b2b73a219fdda
DrangerOG <145707265+DrangerOG@users.noreply.github.com>
2025-04-14 14:18:08 -0700

    Update LogsToggle.tsx

--- NoDelete/src/LogsToggle.tsx
+++ NoDelete/src/LogsToggle.tsx
@@ -1,23 +1,24 @@
+import { FormRow, FormSection } from "enmity/components";
+import { NavigationNative, React } from "enmity/metro/common";
+import { Icons } from "../../common/components/_pluginSettings/utils";
+import Logs from "./Logs";
 
-import { FormRow, FormSection } from 'enmity/components';
-import { NavigationNative, React } from 'enmity/metro/common';
-import { Icons } from '../../common/components/_pluginSettings/utils';
-import Logs from './Logs';
-
-export default ({ styles }) => {
+export default function LogsToggle({ styles }) {
     const Navigation = NavigationNative.useNavigation();
 
-    return <FormSection title="Message Logs">
-        <FormRow
-            label="View Message Logs"
-            subLabel="Tap on an item to copy to clipboard"
-            leading={<FormRow.Icon style={styles.icon} source={Icons.Settings.Debug} />}
-            onPress={() => {
-                Navigation.push("EnmityCustomPage", {
-                    pageName: "NoDelete Message Logs",
-                    pagePanel: Logs
-                })
-            }}
-        />
-    </FormSection>
+    return (
+        <FormSection title="Message Logs">
+            <FormRow
+                label="Open Message Log Viewer"
+                subLabel="Tap to view deleted and edited messages. Long-press to copy."
+                leading={<FormRow.Icon style={styles.icon} source={Icons.Settings.Logs} />}
+                onPress={() => {
+                    Navigation.push("EnmityCustomPage", {
+                        pageName: "NoDelete Logs",
+                        pagePanel: Logs,
+                    });
+                }}
+            />
+        </FormSection>
+    );
 }
